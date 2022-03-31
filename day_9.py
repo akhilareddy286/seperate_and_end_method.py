ec75467b42d89cd0d05ac8d488928dd007faf04b
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-03-31 11:09:16 +0530

    Add files via upload

---
+++ day_9.py
@@ -0,0 +1,60 @@
+"""seperate method"""
+# print('hello' , 'world')
+# print('hello', 'world',sep='')
+# print('hello' ,'world',sep='%')
+# print('a','k','h','i','l','a',sep='*')
+# print('akhila' ,'reddy',sep='' 'ok')
+# print('akhila' ,'reddy',sep=' @$#%! ')
+
+"""end method"""
+
+# print('hello','python',end='')
+# print('hello','python',end='.')
+# print('hi')
+# print('My Name is','Akhila',end=' ')
+# print('lerning','python')
+
+"""combination of sep method and end method"""
+
+# print('hello' ,'python',end='.',sep=' ')
+# print('hello' ,'world','python',end='_',sep='@')
+# print('hello','world','hello','python',end='.',sep=' ')
+
+# print('hello','world','python',sep='$',end='*')
+# print('python','programming',sep=' ',end='.')
+
+# print('python' *5)
+# print('python ' * 5)
+# print('python\n')
+# print('python\n' * 5)
+# print('hello\n',end='')
+# print('hello\n'*3,end='')
+# print('hello\n' *3,end='-')
+# print('akhila\n','reddy',end='c')
+
+# print('a','k','h','i','l','a',end='s',sep='-')
+# print('a','k','r',end='s',sep='-ok')
+# print('a','k','r',end='reddy',sep='b')
+
+# print("hello",end='.' "world")
+# print("hello",end='@' "world")
+# print("hello",end='-' "world")
+
+# print("happy","ugadi",end='.',sep=' ')
+# print("happy","ugadi",end='_',sep='1234')
+
+# print('a','c','r',end='.nd',sep=' 1234ok ')
+# print('a','c','r',end='@ok',sep=' @#$% ')
+# print('a','k','r',end='.cy',sep=' @khil@ ')
+# print('a','c','r',end='.&.',sep='@khi@')
+
+# print('a','k','h','i','l','a',end='@khil@',sep=' rdy ')
+# print('a','k','h','i','l','a',end='5678',sep=' 1234 ') 
+# # print('a','k','h','i','l','a',end='akhi',sep='5678')
+# print('a','k','h','i',end='0123',sep=' ')
+# print('a','k','h','i',end= ' .@khi. ',sep=' .reddy. ' )
+
+# print("core","python",end='.py' "hello")
+# print('core','python',end='-' 'program')
+# print('core','python',end='.',sep=' ' 'program')
+print('core','program',end='_',sep=' .py ')
